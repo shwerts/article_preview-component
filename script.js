6810c5017d49ba470aeb2871fd975c2a4acd1871
shwerts <abidzhanovtemirlan@gmail.com>
2025-03-09 18:18:19 +0500

    Complete mobile layout

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
-let authorBlock = document.getElementsByClassName('article__author')[0];
-let articleFooter = document.getElementsByClassName('article__footer')[0];
-let shareBtn = document.getElementsByClassName('article__shareBtn')[0];
+let articleFooter = document.querySelector('.article__footer');
+let authorBlock = document.querySelector('.article__author');
+let articleLinks = document.querySelector('.article__links');
+let shareBtn = document.querySelector('.article__shareBtn');
+let shareBtnSVG = shareBtn.querySelector('svg');
 
 shareBtn.addEventListener('click', function() {
   switch (authorBlock.style.display) {
     case 'none':
       authorBlock.style.display = 'flex';
+
+      articleLinks.style.display = 'none';
+      
       articleFooter.style.backgroundColor = '#fff';
       articleFooter.style.paddingBlockStart = '10px';
+
+      shareBtn.style.backgroundColor = 'hsl(210, 46%, 95%)';
+      shareBtnSVG.firstElementChild.setAttribute('fill', '#6E8098');
       break;
     default:
       authorBlock.style.display = 'none';
+
+      articleLinks.style.display = 'flex';
+
       articleFooter.style.backgroundColor = 'hsl(217, 19%, 35%)';
       articleFooter.style.paddingBlockStart = '16px';
+
+      shareBtn.style.backgroundColor = 'hsl(214, 17%, 51%)';
+      shareBtnSVG.firstElementChild.setAttribute('fill', '#fff');
   }
 });
